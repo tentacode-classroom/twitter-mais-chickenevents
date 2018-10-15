ab3aded47e7111d5d0e6e9484818d25d01fb9008
Unknown <mottard.maxence@gmail.com>
2018-10-15 12:51:47 +0200

    Add post-new on all pages

--- src/Controller/PostNewController.php
+++ src/Controller/PostNewController.php
@@ -5,19 +5,33 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostNewType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class PostNewController extends AbstractController
 {
     /**
      * @Route("/post/new", name="post_new")
      */
-    public function index()
+    public function index(Request $request, UserInterface $user)
     {
         $post = new Post();
         $form = $this->createForm( PostNewType::class, $post );
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
 
+            $post->setUser( $user );
+            $post->addUserTimeline( $user );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
 
         return $this->render('components/post-new-form.html.twig', [
             'form'  =>  $form->createView()
