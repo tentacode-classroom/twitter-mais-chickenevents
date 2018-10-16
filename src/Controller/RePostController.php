b4bb383139d98fc27e5a6dda5c251022d1aa3ebc
matthieu <matthieu.dabin@ynov.com>
2018-10-16 13:03:41 +0200

    création page retweet et fin edition

---
+++ src/Controller/RePostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+
+
+class RePostController extends AbstractController
+{
+    /**
+     * @Route("/", name="re_post")
+     */
+    public function index(Request $request, UserInterface $user)
+    {
+
+        $post = new Post();
+        if($post->isSubmit()){
+            $post->setUser( $user );
+            $post->addUserTimeline( $user );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+        }
+
+        return $this->render('components/RePost.html.twig', [
+            '' => '',
+        ]);
+    }
+}
